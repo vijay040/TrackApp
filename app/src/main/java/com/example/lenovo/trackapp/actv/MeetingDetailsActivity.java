f1d7228043b854d9b95f1c73948acc3b485eb105
vijay040 <dev.vijayprakash@gamil.com>
2018-06-01 12:46:19 +0530

    this is updated by anuradha

--- app/src/main/java/com/example/lenovo/trackapp/actv/MeetingDetailsActivity.java
+++ app/src/main/java/com/example/lenovo/trackapp/actv/MeetingDetailsActivity.java
@@ -3,15 +3,32 @@ package com.example.lenovo.trackapp.actv;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import com.example.lenovo.trackapp.R;
+import com.example.lenovo.trackapp.model.MeetingModel;
 
 public class MeetingDetailsActivity extends AppCompatActivity {
-
+TextView descreption,purpose,customer,agenda,date,time,address;
+    MeetingModel model;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivity_meetingdetails);
+        descreption=findViewById(R.id.txtdescreption);
+        purpose=findViewById(R.id.txtpurpose);
+        customer=findViewById(R.id.txtcustomer);
+        agenda=findViewById(R.id.txtagenda);
+        date=findViewById(R.id.txtdate);
+        time=findViewById(R.id.txttime);
+        address=findViewById(R.id.txtaddress);
+        descreption.setText(model.getDescreption());
+        purpose.setText(model.getPurpose());
+        customer.setText(model.getCustomer());
+        agenda.setText(model.getAgenda());
+        date.setText(model.getDate());
+        time.setText(model.getTime());
+        address.setText(model.getAddress());
 
 
     }

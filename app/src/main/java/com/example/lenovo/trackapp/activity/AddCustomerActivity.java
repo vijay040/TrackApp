1f1551f7a93b9cdba8a3636e533962d01ec8d904
vijay040 <dev.vijayprakash@gamil.com>
2018-06-05 11:48:30 +0530

    Merge branch 'master' of G:\AndroidProjects\TrackApp\TrackApp with conflicts.

--- app/src/main/java/com/example/lenovo/trackapp/activity/AddCustomerActivity.java
+++ app/src/main/java/com/example/lenovo/trackapp/activity/AddCustomerActivity.java
@@ -1,8 +1,6 @@
 package com.example.lenovo.trackapp.activity;
-
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,20 +11,16 @@ import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.SearchView;
 import android.widget.TextView;
-
 import com.example.lenovo.trackapp.R;
 import com.example.lenovo.trackapp.adaptor.CustomerDetailListAdapter;
 import com.example.lenovo.trackapp.model.CustomerModel;
 import com.example.lenovo.trackapp.model.ResMetaCustomer;
 import com.example.lenovo.trackapp.util.Singleton;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
-
 public class AddCustomerActivity extends AppCompatActivity  implements SearchView.OnQueryTextListener{
     RecyclerView recyclerView;
     List<CustomerModel> clientDetailList = new ArrayList<>();
@@ -40,12 +34,10 @@ public class AddCustomerActivity extends AppCompatActivity  implements SearchVie
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_customer);
         SearchView editText=(SearchView) findViewById(R.id.edt);
-       RelativeLayout addicon=findViewById(R.id.txtAdd);
+        RelativeLayout addicon=findViewById(R.id.txtAdd);
         SearchView editTextName=(SearchView) findViewById(R.id.edt);
         progress = findViewById(R.id.progress);
-
         progress.setVisibility(View.VISIBLE);
-
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         editTextName.setQueryHint("Search By Customer Name ");
         editTextName.setOnQueryTextListener(this);
@@ -67,7 +59,6 @@ public class AddCustomerActivity extends AppCompatActivity  implements SearchVie
         Singleton.getInstance().getApi().getCustomerList("").enqueue(new Callback<ResMetaCustomer>() {
             @Override
             public void onResponse(Call<ResMetaCustomer> call, Response<ResMetaCustomer> response) {
-
                 clientDetailList=response.body().getResponse();
                 manageClientsListAdapter = new CustomerDetailListAdapter(AddCustomerActivity.this, clientDetailList);
                 recyclerView.setAdapter(manageClientsListAdapter);
@@ -77,12 +68,9 @@ public class AddCustomerActivity extends AppCompatActivity  implements SearchVie
             public void onFailure(Call<ResMetaCustomer> call, Throwable t) {
                 Log.e("**Error**", t.getMessage());
                 progress.setVisibility(View.GONE);
-
             }
         });
     }
-
-
     @Override
     public boolean onQueryTextSubmit(String s) {
         return false;
@@ -90,11 +78,11 @@ public class AddCustomerActivity extends AppCompatActivity  implements SearchVie
     @Override
     public boolean onQueryTextChange(String s) {
         s=s.toLowerCase();
-        ArrayList<CustomerModel>newlist=new ArrayList<>();
+        ArrayList<CustomerModel> newlist=new ArrayList<>();
         for(CustomerModel name:clientDetailList)
         {
             String getName=name.getCustomer_name().toLowerCase();
-            if(getName.contains(s)){
+            if(getName.contains(s)) {
                 newlist.add(name);
             }
         }
--- app/src/main/java/com/example/lenovo/trackapp/adaptor/CustomerDetailListAdapter.java
+++ app/src/main/java/com/example/lenovo/trackapp/adaptor/CustomerDetailListAdapter.java
@@ -6,7 +6,6 @@ package com.example.lenovo.trackapp.adaptor;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,7 +27,6 @@ public class CustomerDetailListAdapter extends RecyclerView.Adapter<CustomerDeta
     private Activity context;
     private ProgressDialog progressDialog;
 
-
     public CustomerDetailListAdapter(Activity context, List<CustomerModel> data) {
         this.context = context;
         inflater = LayoutInflater.from(context);
@@ -36,55 +34,51 @@ public class CustomerDetailListAdapter extends RecyclerView.Adapter<CustomerDeta
         progressDialog.setMessage("Please Wait . . .");
         this.data = data;
     }
-
     public void delete(int position) {
         data.remove(position);
         notifyItemRemoved(position);
     }
-
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = inflater.inflate(R.layout.customer_detail_item, parent, false);
         MyViewHolder holder = new MyViewHolder(view);
         return holder;
     }
-
-    public void filter(ArrayList<CustomerModel> newList) {
-        data = new ArrayList<>();
+    public void filter(ArrayList<CustomerModel>newList)
+    {
+        data=new ArrayList<>();
         data.addAll(newList);
         notifyDataSetChanged();
     }
-
     @Override
     public void onBindViewHolder(final MyViewHolder holder, final int position) {
         final CustomerModel current = data.get(position);
-        holder.name.setText("Customer Name: " + current.getCustomer_name());
-        holder.address.setText("Address: " + current.getAddress());
-        holder.email.setText("Email: " + current.getEmail());
-        holder.mobile.setText("Mobile: " + current.getPhone());
-        holder.taxDetail.setText("Tax Details: " + current.getTax_details());
-        holder.companyId.setText("Company Name: " + current.getCompany_name());
+        holder.name.setText("Customer Name: "+current.getCustomer_name());
+        holder.address.setText("Address: "+current.getAddress());
+        holder.email.setText("Email: "+current.getEmail());
+        holder.mobile.setText("Mobile: "+current.getPhone());
+        holder.taxDetail.setText("Tax Details: "+current.getTax_details());
+        holder.companyname.setText("Company Name: "+current.getCompany_name ());
         holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                data.get(position).isVisible = !data.get(position).isVisible;
-                if (data.get(position).isVisible) {
+                data.get(position).isVisible=! data.get(position).isVisible;
+                if(data.get(position).isVisible) {
                     holder.mobile.setVisibility(view.VISIBLE);
                     holder.taxDetail.setVisibility(view.VISIBLE);
-                    holder.companyId.setVisibility(view.VISIBLE);
-                    holder.down.setBackground(ContextCompat.getDrawable(context, R.drawable.ic_up));
-                   // holder.down.setBackgroundResource(R.drawable.ic_up);
-                } else {
+                    holder.companyname.setVisibility(view.VISIBLE);
+                }
+                else
+                {
                     holder.mobile.setVisibility(view.GONE);
                     holder.taxDetail.setVisibility(view.GONE);
-                    holder.companyId.setVisibility(view.GONE);
-                   holder.down.setBackground(ContextCompat.getDrawable(context, R.drawable.ic_down));
-                   // holder.down.setBackgroundResource(R.drawable.ic_down);
+                    holder.companyname.setVisibility(view.GONE);
                 }
             }
         });
 
-        /*if (isMessage)
+
+               /*  if (isMessage)
                     if(current.getTotal_message()!=null)
                     holder.email.setText(current.getTotal_message() + "/2000");
                     else
@@ -97,18 +91,13 @@ public class CustomerDetailListAdapter extends RecyclerView.Adapter<CustomerDeta
                 }
                 holder.title.setText(current.getCreated_at() + " to " + current.getExpire_at());*/
     }
-
-
     @Override
     public int getItemCount() {
         return data.size();
     }
-
     class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView name, address, email, mobile, taxDetail, companyId;
+        TextView name,address,email,mobile,taxDetail,companyname;
         RelativeLayout relativeLayout;
-        ImageView down;
-
         public MyViewHolder(View itemView) {
             super(itemView);
             name = (TextView) itemView.findViewById(R.id.name);
@@ -116,9 +105,8 @@ public class CustomerDetailListAdapter extends RecyclerView.Adapter<CustomerDeta
             email = (TextView) itemView.findViewById(R.id.email);
             mobile = (TextView) itemView.findViewById(R.id.mobile);
             taxDetail = (TextView) itemView.findViewById(R.id.tax_detail);
-            companyId = (TextView) itemView.findViewById(R.id.company_id);
-            down = itemView.findViewById(R.id.imz_down);
-            relativeLayout = (RelativeLayout) itemView.findViewById(R.id.relative_layout_customer);
+            companyname = (TextView) itemView.findViewById(R.id.company_name);
+            relativeLayout=(RelativeLayout)itemView.findViewById(R.id.relative_layout_customer);
         }
     }
 }
